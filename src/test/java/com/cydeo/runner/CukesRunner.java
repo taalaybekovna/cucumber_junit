3f8ab58b574cda076c06a75b9a3bff9cc43b438e
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2023-02-25 21:27:34 -0500

    google_search.feature, webAppLogin.feature

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -15,7 +15,8 @@ import org.junit.runner.RunWith;
             features = "src/test/resources/features" ,
             glue = "com/cydeo/step_definitions",
             dryRun = false,
-            tags = ""
+            tags = "@googleSearch",
+            publish = true
     )
     public class CukesRunner {
     }
